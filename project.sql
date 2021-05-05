aa38fa95e9feb14da61dcaff5689c5102156cd5c
MinhT1 <MinhThienTran1@hotmail.com>
2021-05-05 17:58:53 -0400

    updated sql database

--- project.sql
+++ project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2021 at 03:14 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Generation Time: May 05, 2021 at 09:58 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `project`
 --
-CREATE DATABASE IF NOT EXISTS `project` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `project`;
 
 -- --------------------------------------------------------
 
@@ -33,8 +31,19 @@ CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
   `profile_id` int(11) NOT NULL,
   `payment_confirmation` varchar(64) DEFAULT NULL,
-  `status` enum('cart','paid','shipped','') NOT NULL
+  `status` enum('cart','paid','shipped','delivered') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `profile_id`, `payment_confirmation`, `status`) VALUES
+(9, 3, 'confirmed', 'delivered'),
+(10, 3, 'confirmed', 'paid'),
+(11, 4, 'confirmed', 'paid'),
+(12, 3, 'confirmed', 'paid'),
+(13, 3, 'Not confirmed', 'cart');
 
 -- --------------------------------------------------------
 
@@ -49,6 +58,19 @@ CREATE TABLE `orders_detail` (
   `quantity` int(11) NOT NULL,
   `price` decimal(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders_detail`
+--
+
+INSERT INTO `orders_detail` (`orders_detail_id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
+(28, 9, 1, 1, '5.00'),
+(29, 9, 3, 2, '1.00'),
+(30, 11, 1, 1, '5.00'),
+(31, 11, 3, 1, '1.00'),
+(32, 11, 4, 1, '3.00'),
+(33, 10, 1, 5, '5.00'),
+(34, 12, 1, 5, '5.00');
 
 -- --------------------------------------------------------
 
@@ -71,9 +93,9 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `name`, `description`, `image`, `qoh`, `price`, `sales`) VALUES
-(1, 'Milk', 'Nothing compares to a cold glass of fresh Natrel Milk.', '608f196a7c451.jpg', 10, '5.00', 0),
-(3, 'Water', 'The 1.5 L bottle is a great way to make sure your family always.', '608f198416a56.jpg', 30, '1.00', 0),
-(4, 'Juice', 'Minute Maid Light Lemonade. A Squeeze Above The Rest.', '608f188a3b2b2.jpg', 15, '3.00', 0),
+(1, 'Milk', 'Nothing compares to a cold glass of fresh Natrel Milk.', '608f196a7c451.jpg', 0, '5.00', 12),
+(3, 'Water', 'The 1.5 L bottle is a great way to make sure your family always.', '608f198416a56.jpg', 30, '1.00', 3),
+(4, 'Juice', 'Minute Maid Light Lemonade. A Squeeze Above The Rest.', '608f188a3b2b2.jpg', 15, '3.00', 1),
 (5, 'Apple', 'Great for snacking, sauces, salads and baking.', '608f18e177fcd.jpg', 25, '1.00', 0),
 (6, 'Banana', 'Soft, sweet and delicious, the banana is a popular choice.', '608f13b49b631.jpg', 15, '1.00', 0),
 (7, 'Orange', 'Sweet, juicy and seedless.', '608f13e6d9063.jpg', 15, '1.00', 0),
@@ -100,7 +122,9 @@ CREATE TABLE `profile` (
 
 INSERT INTO `profile` (`profile_id`, `user_id`, `first_name`, `last_name`, `phone`, `address`) VALUES
 (1, 1, 'Admin', 'Admin', '514-123-4567', 'Vanier College'),
-(2, 2, 'Jhann', 'Quiambao', '514-123-4567', 'Vanier College');
+(2, 2, 'Jhann', 'Quiambao', '514-123-4567', 'Vanier College'),
+(3, 3, 'Minh', 'Thien', '5145760435', '6390 Av Mountain Sights'),
+(4, 4, 'Tran', 'Tran', '1234567890', 'somewhere');
 
 -- --------------------------------------------------------
 
@@ -114,6 +138,13 @@ CREATE TABLE `review` (
   `rating` varchar(16) NOT NULL,
   `review` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `review`
+--
+
+INSERT INTO `review` (`profile_id`, `product_id`, `rating`, `review`) VALUES
+(3, 1, '5', 'asdasd');
 
 -- --------------------------------------------------------
 
@@ -134,7 +165,9 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `username`, `password_hash`, `role`) VALUES
 (1, 'admin', '$2y$10$l/WJ2iUgqfN7P.euKze4Z.0hLE36nfYmAmArOaR0MpgL0BQqmJwxe', 'admin'),
-(2, 'jhann', '$2y$10$wG/dmGYEIv/hivdLVBk4s.PYkW3oU18d9/cm/UDpkm2H4kx0u3aqO', 'user');
+(2, 'jhann', '$2y$10$wG/dmGYEIv/hivdLVBk4s.PYkW3oU18d9/cm/UDpkm2H4kx0u3aqO', 'user'),
+(3, 'MinhThien', '$2y$10$LHSbz4WNPHXUrTHnZ5UNZ.6BXP5jmNsNvX4uYBp3AMnQhUO2OLnxm', 'user'),
+(4, 'Tran', '$2y$10$iTLHxICbj4rGOhuQLPB68O64dt5kJOsVYVLbk0KgW.YgR45gOa4ZC', 'user');
 
 --
 -- Indexes for dumped tables
@@ -189,13 +222,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `orders_detail`
 --
 ALTER TABLE `orders_detail`
-  MODIFY `orders_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `orders_detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `product`
@@ -207,13 +240,13 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
