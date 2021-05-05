c2bdd7f496eac522a6510b685f759d835d18befd
MinhT1 <MinhThienTran1@hotmail.com>
2021-05-05 18:25:50 -0400

    new db with object creation

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2021 at 09:58 PM
+-- Generation Time: May 05, 2021 at 10:24 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `project`
 --
+CREATE DATABASE IF NOT EXISTS `project` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `project`;
 
 -- --------------------------------------------------------
 
